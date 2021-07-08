216b323df0dd6a7a3df2edaec91c2df5e4b8c5e5
artha <artha.djoe@gmail.com>
2021-07-08 21:39:17 +0200

    added step2

--- api/server.js
+++ api/server.js
@@ -2,25 +2,39 @@ const countries = require('./countriesData.json')
 const express = require('express');
 const PORT = 8000;
 const app = express();
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
-
-
-
-app.get('/all', (req, res) => {
-    let countryList = []
-    for (let i = 0 ; i < countries.length ; i++) {
-        countryList.push(countries[i].name)
-    }
-    
-    res.json(countryList);
+app.get("/all", (req, res) => {
+	res.json({
+		data: countries,
+	});
 });
 
 
 
+app.get("/country/:countryName", (req, res) => {
+	const countryName = req.params.countryName.toLowerCase();
+	const countryData = countries.filter(
+		(country) => 
+        country.name.toLowerCase() === countryName
+    );
+	res.json({
+		data: countryData,
+	});
+});
 
+app.get("/capital/:capitalName", (req, res) => {
+	const capitalName = req.params.capitalName.toLowerCase();
+	const countryData = countries.filter(
+		(country) => country.capital.toLowerCase() === capitalName
+    );
+	res.json({
+		data: countryData,
+	});
+});
 
 app.listen(PORT, () => {
   console.log('Server started on port: ' + PORT);
 });
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,41 @@
-getAllCountries()
+$(() => {
+	getAllCountries();
+	$("#btnShowData").click(handleClick);
+});
+
 
 
 async function getAllCountries() {
-    const res = await fetch('http://localhost:8000/all')
-    const countries = await res.json()
+    const res = await fetch('http://localhost:8000/all');
+    const jsonRes = await res.json();
    
-    countriesList(countries.data)
-    
-    /*
-    for (let i=0 ; i < data.length ; i++) {
-        const li = document.createElement('li')
-        ul.appendChild(li)
-        li.innerHTML=li.innerHTML + data[i];
-    }
-    */
-}
+    countriesList(jsonRes.data);
+};
 
 function countriesList(list) {
+    $("#list").empty();
 	list.forEach((country) =>
 		$("#list").append(`<li><p>${country.name} - ${country.capital}</p></li>`));
-}
+};
 
+async function handleClick() {
+    const submitValue = $("#submitValue").val();
+    if ($('#country').is(':checked')) {
+        const res = await fetch(`http://localhost:8000/country/${submitValue}`);
+        const jsonRes = await res.json();
+        console.log("this is country", jsonRes);
+        countriesList(jsonRes.data);
+        if (jsonRes.data.length === 0) {
+            alert("error: there is no country with that name")
+        }
+    } else
+    if ($('#capital').is(':checked')) {
+        const res = await fetch(`http://localhost:8000/capital/${submitValue}`);
+        const jsonRes = await res.json();
+        console.log("this is capital", jsonRes);
+        countriesList(jsonRes.data);
+        if (jsonRes.data.length === 0) {
+            alert("error: there is no capital with that name")
+        }
+    };
+};
