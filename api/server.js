246a7ecf50db25c2685fe67462ac54b9dc4ae743
artha <artha.djoe@gmail.com>
2021-07-09 11:47:19 +0200

    added step3

--- api/server.js
+++ api/server.js
@@ -13,8 +13,8 @@ app.get("/all", (req, res) => {
 
 
 
-app.get("/country/:countryName", (req, res) => {
-	const countryName = req.params.countryName.toLowerCase();
+app.get("/country/:id", (req, res) => {
+	const countryName = req.params.id.toLowerCase();
 	const countryData = countries.filter(
 		(country) => 
         country.name.toLowerCase() === countryName
@@ -24,10 +24,20 @@ app.get("/country/:countryName", (req, res) => {
 	});
 });
 
-app.get("/capital/:capitalName", (req, res) => {
-	const capitalName = req.params.capitalName.toLowerCase();
+app.get("/capital/:id", (req, res) => {
+	const capitalName = req.params.id.toLowerCase();
 	const countryData = countries.filter(
 		(country) => country.capital.toLowerCase() === capitalName
+    );
+	res.json({
+		data: countryData,
+	});
+});
+
+app.get("/region/:id", (req, res) => {
+	const regionName = req.params.id.toLowerCase();
+	const countryData = countries.filter(
+		(country) => country.region.toLowerCase() === regionName
     );
 	res.json({
 		data: countryData,
--- front/js/script.js
+++ front/js/script.js
@@ -15,27 +15,40 @@ async function getAllCountries() {
 function countriesList(list) {
     $("#list").empty();
 	list.forEach((country) =>
-		$("#list").append(`<li><p>${country.name} - ${country.capital}</p></li>`));
+		$("#list").append(`<li><p>${country.name} - ${country.capital}</p></li>`)
+    );
 };
 
 async function handleClick() {
-    const submitValue = $("#submitValue").val();
+    
     if ($('#country').is(':checked')) {
+        const submitValue = $("#submitValue").val();
         const res = await fetch(`http://localhost:8000/country/${submitValue}`);
         const jsonRes = await res.json();
         console.log("this is country", jsonRes);
         countriesList(jsonRes.data);
         if (jsonRes.data.length === 0) {
-            alert("error: there is no country with that name")
-        }
+            alert("error: there is no country with that name");
+        };
     } else
     if ($('#capital').is(':checked')) {
+        const submitValue = $("#submitValue").val();
         const res = await fetch(`http://localhost:8000/capital/${submitValue}`);
         const jsonRes = await res.json();
         console.log("this is capital", jsonRes);
         countriesList(jsonRes.data);
         if (jsonRes.data.length === 0) {
-            alert("error: there is no capital with that name")
-        }
+            alert("error: there is no capital with that name");
+        };
+    } else
+    if ($('#region').is(':checked')) {
+        const submitregions = $("#regionInput").val();
+        const res = await fetch(`http://localhost:8000/region/${submitregions}`);
+        const jsonRes = await res.json();
+        console.log("this is region", $("#regionInput").val(), jsonRes);
+        countriesList(jsonRes.data);
+        if (jsonRes.data.length === 0) {
+            alert("error: there is no region with that name");
+        };
     };
 };
